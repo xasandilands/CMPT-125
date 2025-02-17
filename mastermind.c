cbe410339f66421cf1a0c65479af6e6fbbf2bc9d
Xavier <njsandilands@gmail.com>
2025-02-16 19:09:21 -0800

    Get Seed
    Gets seed input from user and checks if value is NULL. If value is NULL the program asks the user indefinitely until seed is equal to an int value

--- mastermind.c
+++ mastermind.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
+int main()
+{
+    char SOLUTION[6];
+    char PREVIOUS_GUESS[20][6];
+    char GUESS[6];
+    int ATTEMPTS = 0;
+    int SEED = NULL;
+    
+    printf("Enter the integer value of the seed for the game: ");
+    scanf("%d", &SEED);
+    while(SEED == NULL)
+    {
+        printf("Try again you made an error");
+        printf("Please enter an integer to be the seed for the game: ");
+        scanf("%d", &SEED);
+    }
+    
+    
+
+}
